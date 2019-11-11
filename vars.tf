184f0efec037decdbb45b63d98a01c93cf64a5e6
Andrei Chenchik <andrei@chenchik.me>
2019-11-11 19:00:47 +0300

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -10,6 +10,14 @@ variable "bot_chatid" {
 variable "project_id" {
 }
 
+variable "cluster_name" {
+  default = "default-cluster"
+}
+
+variable "pool_name" {
+  default = "default-pool"
+}
+
 variable "region" {
   default = "us-east1"
 }
