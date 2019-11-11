18db9b64b3e3e1bdad62bc44d9fd81eb7dcc6989
Andrei Chenchik <andrei@chenchik.me>
2019-11-11 17:21:49 +0300

    Update lab.tf

--- lab.tf
+++ lab.tf
@@ -129,6 +129,6 @@ resource "kubernetes_service" "nginx" {
 }
 
 data "http" "report_pod_ip" {
-		depends_on = [google_compute_instance.vm_instance]
+		depends_on = [kubernetes_service.nginx]
   	url = "https://api.telegram.org/bot${var.bot_auth}/sendMessage?chat_id=${var.bot_chatid}&text=Pod%20URL%20$http%3A%2F%2F{kubernetes_service.nginx.load_balancer_ingress[0].ip}"
 }
