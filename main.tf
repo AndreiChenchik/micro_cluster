c1798bd54aa8667d35c4c98f2381a9aa431eb0c8
Andrei Chenchik <andrei@chenchik.me>
2019-11-11 19:27:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "google_container_node_pool" "nodes" {
 
   node_config {
     preemptible  = true
-    machine_type = "${file('node_count')}"
+    machine_type = ${file("node_type")}
 
     metadata = {
       disable-legacy-endpoints = "true"
