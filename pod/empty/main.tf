aa5a6b62e8b7bd9e2b7f1c1174e6250b1e0eef2d
Andrei Chenchik <andrei@chenchik.me>
2019-11-11 20:40:05 +0300

    Delete main.tf

--- pod/empty/main.tf
+++
@@ -1,9 +0,0 @@
-variable "bot_auth" {
-}
-
-variable "bot_chatid" {
-}
-
-data "http" "report_pod_ip" {
-  url = "https://api.telegram.org/bot${var.bot_auth}/sendMessage?chat_id=${var.bot_chatid}&text=Pods%20removed"
-}
