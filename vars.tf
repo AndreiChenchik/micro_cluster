371c90ef3f866f38ecc64ef4b0f6cb996868617b
Andrei Chenchik <andrei@chenchik.me>
2019-11-11 17:46:39 +0300

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,20 @@
+variable "credentials" {
+}
+
+variable "bot_auth" {
+}
+
+variable "bot_chatid" {
+}
+
+variable "project_id" {
+}
+
+variable "region" {
+}
+
+variable "zone" {
+}
+
+variable "instance_name" {
+}
