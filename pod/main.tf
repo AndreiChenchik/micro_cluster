fb17251e710233d134b25c0db769719b9c5e091e
Andrei Chenchik <andrei@chenchik.me>
2019-11-11 19:55:29 +0300

    Update main.tf

--- pod/main.tf
+++ pod/main.tf
@@ -1,4 +1,3 @@
-/*
 resource "kubernetes_pod" "nginx" {
   metadata {
     name = "nginx-example"
@@ -31,8 +30,9 @@ resource "kubernetes_service" "nginx" {
     type = "LoadBalancer"
   }
 }
+
 data "http" "report_pod_ip" {
   depends_on = [kubernetes_service.nginx]
   url = "https://api.telegram.org/bot${var.bot_auth}/sendMessage?chat_id=${var.bot_chatid}&text=Pod%20URL%20http%3A%2F%2F${kubernetes_service.nginx.load_balancer_ingress[0].ip}"
 }
-*/
+
