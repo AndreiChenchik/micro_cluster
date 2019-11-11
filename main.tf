5fe5ba22c66910b72e24ca8831b2b90dccdba9d7
Andrei Chenchik <andrei@chenchik.me>
2019-11-11 18:53:30 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,11 +54,12 @@ resource "google_container_node_pool" "nodes" {
 # Query my Terraform service account from GCP
 data "google_client_config" "current" {}
 
-data "google_container_cluster" "my-gke-cluster" {
-  name = "my-gke-cluster"
-  location = "${var.zone}"
+provider "kubernetes" {
+  load_config_file = false
+  host = "https://${google_container_cluster.primary.endpoint}"
+  cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
+  token = "${data.google_client_config.current.access_token}"
 }
-/*
 
 provider "kubernetes" {
   load_config_file = false
@@ -67,6 +68,7 @@ provider "kubernetes" {
   token = "${data.google_client_config.current.access_token}"
 }
 
+/*
 resource "kubernetes_pod" "nginx" {
   metadata {
     name = "nginx-example"
