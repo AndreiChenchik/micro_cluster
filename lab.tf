b10759c9c296adb2484e59a19d5ecd4de96f0fcd
Andrei Chenchik <andrei@chenchik.me>
2019-11-11 17:11:47 +0300

    Update lab.tf

--- lab.tf
+++ lab.tf
@@ -87,7 +87,7 @@ data "google_client_config" "current" {}
 provider "kubernetes" {
   load_config_file = false
   host = "https://${google_container_cluster.primary.endpoint}"
-  cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.cluster_ca_certificate)}"
+  cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
   token = "${data.google_client_config.current.access_token}"
 }
 
