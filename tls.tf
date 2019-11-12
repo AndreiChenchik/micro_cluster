3c2fc5bb1c3e51ce38a60f32fa04f43539245cb7
Andrei Chenchik <andrei@chenchik.me>
2019-11-12 23:17:32 +0300

    Update tls.tf

--- tls.tf
+++ tls.tf
@@ -8,7 +8,7 @@ resource "tls_private_key" "private_key" {
 
 resource "acme_registration" "reg" {
   account_key_pem = "${tls_private_key.private_key.private_key_pem}"
-  email_address   = "andrei@chenchik.me"
+  email_address   = "${var.email}"
 }
 
 resource "acme_certificate" "certificate" {
