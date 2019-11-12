1b282fc819838ab3d71e1955f1083c1d3e1c15da
Andrei Chenchik <andrei@chenchik.me>
2019-11-12 08:36:41 +0300

    Create container-vars.tf

---
+++ container-vars.tf
@@ -0,0 +1,18 @@
+variable "app_name" {
+  default="cloud-jupyter"
+  }
+  
+variable "persistent-disk-name" {
+  }
+  
+variable "docker_image" {
+default="docker.pkg.github.com/gumlooter/lab/jupyter:20191023162926d64653"
+  }
+  
+variable "mount_path" {
+default="/home/jovyan/work"
+  }
+  
+variable "exposed_port" {
+default="8888"
+}
