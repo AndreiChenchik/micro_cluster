7e9d77a4df7225fcacc97fa7a62e3b3925291859
Andrei Chenchik <andrei@chenchik.me>
2019-11-12 13:38:34 +0300

    Update container.tf

--- container.tf
+++ container.tf
@@ -12,7 +12,7 @@ resource "google_dns_record_set" "a-record" {
 
   managed_zone = "${var.dns-zone-name}"
 
-  rrdatas = ["${kubernetes_ingress.ingress[0].load_balancer_ingress[0].ip}"]
+  rrdatas = ["${kubernetes_service.loadbalancer[0].load_balancer_ingress[0].ip}"]
 }
 
 resource "kubernetes_pod" "container" {
@@ -36,6 +36,10 @@ resource "kubernetes_pod" "container" {
         name = var.envs[0].name
         value = var.envs[0].value
         }
+      env {
+        name = var.envs[1].name
+        value = var.envs[1].value
+        }
       command = [var.command]
       args = local.args
       volume_mount {
@@ -53,71 +57,26 @@ resource "kubernetes_pod" "container" {
   }
 }
 
-resource "google_compute_managed_ssl_certificate" "container" {
+resource "kubernetes_service" "loadbalancer" {
   count = local.node_count != 1 ? 0 : 1
-
-  provider = "google-beta"
-
-  name = "container-ssl"
-
-  managed {
-    domains = ["{var.dns-subdomain}.${var.dns-zone}"]
-  }
-}
-
-
-resource "kubernetes_service" "proxy" {
-  count = local.node_count != 1 ? 0 : 1
-
+  
+  depends_on = [kubernetes_pod.container]
   metadata {
-    namespace = "default"
-    name      = "container-proxy"
+    name = "loadbalancer"
   }
-
   spec {
-    type             = "NodePort"
-    session_affinity = "ClientIP"
-
-    port {
-      name        = "http"
-      protocol    = "TCP"
-      port        = 80
-      target_port = 8888
-    }
-
     selector = {
-      app = kubernetes_pod.container[0].metadata[0].labels.App
+      App = kubernetes_pod.container[0].metadata[0].labels.App
     }
-  }
-}
-
-resource "kubernetes_ingress" "ingress" {
-  count = local.node_count != 1 ? 0 : 1
-
-  metadata {
-    name = "container-ingress"
-
-    annotations = {
-      "ingress.gcp.kubernetes.io/pre-shared-cert"   = google_compute_managed_ssl_certificate.container[0].name
-      "kubernetes.io/ingress.global-static-ip-name" = google_compute_global_address.address.name
+    port {
+      port        = var.external_port
+      target_port = var.container_port
     }
-  }
-
-  spec {
-    rule {
-      http {
-        path {
-          backend {
-            service_name = kubernetes_service.proxy[0].metadata.0.name
-            service_port = 80
-          }
-        }
-      }
-    }
+    type = "LoadBalancer"
   }
 }
 
 data "http" "report_pod_ip" {
-  depends_on = [kubernetes_ingress.ingress]
+  depends_on = [kubernetes_service.loadbalancer]
   url = "https://api.telegram.org/bot${var.bot_auth}/sendMessage?chat_id=${var.bot_chatid}&text=${urlencode(local.action)}"
 }
