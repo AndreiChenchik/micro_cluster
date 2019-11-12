79f9ae2c5a6903df7eb2a7890669140ac7f72e19
Andrei Chenchik <andrei@chenchik.me>
2019-11-12 08:37:03 +0300

    Update container-vars.tf

--- container-vars.tf
+++ container-vars.tf
@@ -6,13 +6,13 @@ variable "persistent-disk-name" {
   }
   
 variable "docker_image" {
-default="docker.pkg.github.com/gumlooter/lab/jupyter:20191023162926d64653"
+  default="docker.pkg.github.com/gumlooter/lab/jupyter:20191023162926d64653"
   }
   
 variable "mount_path" {
-default="/home/jovyan/work"
+  default="/home/jovyan/work"
   }
   
 variable "exposed_port" {
-default="8888"
-}
+  default="8888"
+  }
