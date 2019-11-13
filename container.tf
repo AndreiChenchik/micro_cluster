09524c5c3af5b11d867c2b43f069b9263fd4f6ec
Andrei Chenchik <andrei@chenchik.me>
2019-11-13 17:52:29 +0300

    Update container.tf

--- container.tf
+++ container.tf
@@ -42,6 +42,6 @@ resource "kubernetes_pod" "container" {
   }
 
 data "http" "report_pod_ip" {
-  depends_on = [kubernetes_ingress.loadbalancer]
+  depends_on = [kubernetes_service.loadbalancer]
   url = "https://api.telegram.org/bot${var.bot_auth}/sendMessage?chat_id=${var.bot_chatid}&text=${urlencode(local.action)}"
 }
