144089c40fa9a4e1411a2df0dbb29d7047641ae2
Andrei Chenchik <andrei@chenchik.me>
2019-11-13 17:48:43 +0300

    Update proxy.tf

--- proxy.tf
+++ proxy.tf
@@ -1,39 +1,4 @@
-resource "kubernetes_ingress" "ingress" {
-  count = local.node_count != 1 ? 0 : 1
-
-  metadata {
-    name = "${kubernetes_pod.container[0].metadata.0.labels.app}"
-    
-    annotations = {
-      "kubernetes.io/ingress.global-static-ip-name" = "${google_compute_global_address.static[0].name}"
-      "ingress.kubernetes.io/force-ssl-redirect" = "true"
-    }
-  }
-
-  spec {
-    rule {
-      host = "${var.dns-subdomain}.${var.dns-zone}"
-      http {
-        path {
-          path = "/"
-          backend {
-            service_name = "${kubernetes_pod.container[0].metadata.0.labels.app}"
-            service_port = var.container_port
-            }
-          }
-        }
-      }
-    
-    tls {
-      hosts = ["${var.dns-subdomain}.${var.dns-zone}"]
-      secret_name = "tls-cert"
-    }
-  }
-  depends_on = [kubernetes_service.nodeport]
-}
-
-
-resource "kubernetes_service" "nodeport" {
+resource "kubernetes_service" "loadbalancer" {
   count = local.node_count != 1 ? 0 : 1
   
   metadata {
@@ -45,8 +10,9 @@ resource "kubernetes_service" "nodeport" {
     }
     port {
       port = var.container_port
+      target_port = var.external_port
     }
 
-  type = "NodePort"
+  type = "LoadBalancer"
   }
 }
