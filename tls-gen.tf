26bde4bc1170e4e09e077ab21db1a39e1cb231fc
Andrei Chenchik <andrei@chenchik.me>
2019-12-06 15:52:33 +0300

    Update tls-gen.tf

--- tls-gen.tf
+++ tls-gen.tf
@@ -7,20 +7,20 @@ resource "tls_private_key" "private_key" {
 }
 
 resource "acme_registration" "reg" {
-  account_key_pem = "${tls_private_key.private_key.private_key_pem}"
-  email_address   = "${var.email}"
+  account_key_pem = tls_private_key.private_key.private_key_pem
+  email_address   = var.email
 }
 
 resource "acme_certificate" "cert" {
-  account_key_pem           = "${acme_registration.reg.account_key_pem}"
+  account_key_pem           = acme_registration.reg.account_key_pem
   common_name               = "${var.dns-subdomain}.${var.dns-zone}"
 
   dns_challenge {
     provider = "gcloud"
     
     config = {
-      GCE_PROJECT = "${var.project_id}"
-      GCE_SERVICE_ACCOUNT = "${var.credentials}"
+      GCE_PROJECT = var.project_id
+      GCE_SERVICE_ACCOUNT = var.credentials
       }
   }
 }
