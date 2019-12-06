7f539ffa217776b78fabb83159bd5bff24fe8cfb
Andrei Chenchik <andrei@chenchik.me>
2019-12-06 13:01:45 +0300

    Update main.tf

--- jupyter/main.tf
+++ jupyter/main.tf
@@ -1,7 +1,25 @@
-# Jupyter Notebook
+# HOW TO USE:
+# add following to your terraform config
+#module "jupyter" {
+#  source = "git@github.com:gumlooter/dockerized_jupyter.git"
+#  count = 1 # 0 to turn it off
+#  node_pool = google_container_node_pool.nodes
+#  persistent_disk = "development-storage"
+#  external_port = 443
+#  public_url = "http://jupyter.example.com"
+#  password = "sha1:74ba40f8a388:c913541b7ee99d15d5ed31d4226bf7838f83a50e"
+#}
+
+# calculate local vars based on input vars
+locals {
+  # decide to run or not to run based on count input
+  onoff_switch = var.count != 1 ? 0 : 1
+}
+
+# schedule Jupyter Notebook
 resource "kubernetes_deployment" "jupyter_deployment" {
   # create resource only if there it's required
-  count = var.onoff_switch
+  count = local.onoff_switch
   
   # wait for gke node pool
   depends_on = [var.node_pool]
@@ -59,7 +77,7 @@ resource "kubernetes_deployment" "jupyter_deployment" {
   }
 }
 
-# load balancer to drive external traffic to pod
+# add load balancer to drive external traffic to pod
 resource "kubernetes_service" "jupyter_loadbalancer" {
   # create resource only if there it's required
   count = var.onoff_switch
