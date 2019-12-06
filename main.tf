913c2127a31a586b817e2addc078a3c6f87248b8
Andrei Chenchik <andrei@chenchik.me>
2019-12-06 18:37:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "google_compute_firewall" "default" {
 
 # get ips
 locals {                                                            
-  ip = local.node_count != 1 ? "127.0.0.1" : data.google_compute_instance.node_info[0].network_interface.access_config.nat_ip          
+  ip = data.google_compute_instance.node_info != [] ? data.google_compute_instance.node_info[0].network_interface.access_config.nat_ip : "127.0.0.1"
 }  
   
 # assign dns name  
