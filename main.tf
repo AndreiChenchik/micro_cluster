69fc8b8c29668f2b60358952a9da7f970e403816
Andrei Chenchik <andrei@chenchik.me>
2019-12-06 17:43:07 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -104,6 +104,6 @@ data "google_compute_instance_group" "pool_info" {
 }
   
 # get first node info
-data "google_compute_instance_group" "node_info" {
-  self_link = google_compute_instance_group.pool_info.instances[0]
+data "google_compute_instance" "node_info" {
+  self_link = data.google_compute_instance_group.pool_info.instances[0]
 }
