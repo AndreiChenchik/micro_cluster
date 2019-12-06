a6367f748165b9cf79f9e3a11afb05c6aa2983ab
Andrei Chenchik <andrei@chenchik.me>
2019-12-06 10:52:00 +0300

    Create main.tf

---
+++ jupyter/main.tf
@@ -0,0 +1,58 @@
+# Calculate args variable
+locals {
+  args = concat(var.args, ["--NotebookApp.custom_display_url=${var.public_url}","--NotebookApp.password=${var.password}"])
+}
+
+# Jupyter Notebook
+resource "kubernetes_pod" "jupyter_deployment" {
+  # create resource only if there is any nodes
+  count = var.onoff_switch
+  
+  # wait for gke node pool
+  depends_on = [var.node_pool]
+
+  # lable to let service discover the pod
+  metadata {
+    labels = {
+      app = var.command
+    }
+  }
+
+  spec {
+    container {
+      image = var.image
+      command = [var.command]
+      args = local.args      
+      
+      # all the jupyter settings
+      env {
+        name = var.envs[0].name
+        value = var.envs[0].value
+      }     
+      
+      # expose ports
+      port {
+        container_port = var.main_port
+      }
+
+      # attach persistent-disk to node
+      volume {
+        name= "persistent-volume"
+        gce_persistent_disk {
+          pd_name = var.persistent_disk
+        }
+      }
+
+      # mount disk to container
+      volume_mount {
+        mount_path = var.persistent_mount_path
+        name = "persistent-volume"
+      }      
+    }
+    
+    # terraform: give container more time to load image (it's huge)
+    timeouts {
+      create = var.terraform_timeout
+    }
+  }
+}
