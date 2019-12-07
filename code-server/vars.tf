316f0a4cba6598b83ea69dc00c96e47d9e6042b0
Andrei Chenchik <andrei@chenchik.me>
2019-12-07 14:31:21 +0300

    Create vars.tf

---
+++ code-server/vars.tf
@@ -0,0 +1,43 @@
+# external variables 
+variable "module_count" {}
+variable "node_pool" {}
+variable "password" {}
+variable "persistent_disk" {}
+variable "external_port" {}
+
+#internal variables
+variable "app_name" {
+  default="code-server"
+}
+
+variable "container_name" {
+  default="code-server-container"
+}
+
+variable "deployment_name" {
+  default="code-server-deployment"
+}
+
+variable "image" {
+  default="codercom/code-server:v2"
+}
+  
+variable "persistent_mount_path" {
+  default="/home"
+}
+
+variable "main_port" {
+  default = "8889"
+}
+
+variable "command" {
+  default = [
+    "code-server"
+  ]
+}
+
+variable "args" {
+  default = [
+    "--cert"
+  ]
+}
