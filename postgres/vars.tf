a9e43b65f7a60be1572efe01d198b147f4ae702e
Andrei Chenchik <andrei@chenchik.me>
2019-12-07 11:03:20 +0300

    Update and rename jupyter/vars.tf to postgres/vars.tf

---
+++ postgres/vars.tf
@@ -0,0 +1,24 @@
+# external variables 
+variable "onoff_switch" {}
+variable "node_pool" {}
+variable "password" {}
+variable "user" {}
+variable "persistent_disk" {}
+variable "external_port" {}
+
+#internal variables
+variable "app_name" {
+  default="postgres"
+}
+
+variable "image" {
+  default="postgres:latest"
+}
+  
+variable "persistent_mount_path" {
+  default="/var/lib/postgresql/data/pgdata"
+}
+
+variable "main_port" {
+  default = "5432"
+}
