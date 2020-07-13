291bf194120eaf55502d950542dac4a3388e4418
Andrei <andrei@chenchik.me>
2020-07-13 07:38:39 +0300

    Create vars.tf

---
+++ finance-automation/vars.tf
@@ -0,0 +1,34 @@
+# external variables 
+variable "module_count" {}
+variable "node_pool" {}
+variable "external_port" {}
+variable "dockerconfigjson" {}
+
+#internal variables
+variable "name" {
+  default="finance-automation"
+}
+
+variable "image" {
+  default="gumlooter/finance:latest"
+}
+
+variable "main_port" {
+  default = "8899"
+}
+
+variable "command" {
+  default = [
+    "python"
+  ]
+}
+
+variable "args" {
+  default = [
+    "finance.py"
+  ]
+}
+
+variable "terraform_timeout" {
+  default = "10m"
+}
